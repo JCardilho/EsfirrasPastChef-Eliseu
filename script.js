7d5abdcb3c0405508bd2c8ab14736762b3c0df66
JCardilho <jose.1433@hotmail.com>
2024-04-02 17:54:41 -0300

    fead inicio cart

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log('Hello, World!');
+const menu = document.getElementById("menu");
+const cartBtn = document.getElementById("cart-btn");
+const cartModal = document.getElementById("cart-modal");
+const cartItemsContainer = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const checkoutBtn = document.getElementById("checkout-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const cartCounter = document.getElementById("cart-count");
+const addressInput = document.getElementById("address");
+const addressWarn = document.getElementById("address-warn");
+
+let cart = [];
+
+cartBtn.addEventListener("click", function () {
+  cartModal.style.display = "flex";
+});
+cartModal.addEventListener("click", function (event) {
+  if (event.target === cartModal) {
+    cartModal.style.display = "none";
+  }
+})
+closeModalBtn.addEventListener("click", function () {
+  cartModal.style.display = "none"
+})
+menu.addEventListener("click", function (event) {
+  
+  let parentButton = event.target.closest(".add-to-cart-btn")
+
+  if (parentButton) {
+    const name = parentButton.getAttribute("data-name")
+    const price = parseFloat(parentButton.getAttribute("data-price"))
+   
+    addToCart(name, price)
+  }
+});
+
+function addToCart(name, price) {
+  cart.push({
+    name,
+    price,
+    quantity: 1,
+  });
+}
