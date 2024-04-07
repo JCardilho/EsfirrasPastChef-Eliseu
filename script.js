531442a29f5222b1adab7fec19796fe49d1c1e88
JCardilho <115733809+JCardilho@users.noreply.github.com>
2024-04-07 08:36:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -103,9 +103,24 @@ function removeItemCart(name){
     }
 
     cart.splice(index,1);
-    updateCartModal():
+    updateCartModal();
   }
 }
 
-addressInput.addEventListener("input", fu)
+addressInput.addEventListener("input", function(event){
+  let inputValue = event.target.value;
+  if(inputValue !== ""){
+    addressInput.classList.remove("border-red-500")
+    addressWarn.classList.remove("hidden")
+  }
+})
+
+checkoutBtn.addEventListener("click", function(){
+  if(cart.length === 0)return;
+  if(addressInput.value === ""){
+    addressWarn.classList.remove("hidden")
+    addressInput.classList.add("border-red-500")
+    return;
+  }
+})
 
