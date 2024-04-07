d380f0ae5b7ca67a81a83dbb6cc8acb8391230d8
JCardilho <115733809+JCardilho@users.noreply.github.com>
2024-04-07 14:06:39 -0300

    fead "toast"

--- script.js
+++ script.js
@@ -116,11 +116,63 @@ addressInput.addEventListener("input", function(event){
 })
 
 checkoutBtn.addEventListener("click", function(){
+  const isOpen = checkRestaurantOpen();
+  if(isOpen){
+    
+    Toastify({
+      text: "Ops Fechado",
+      duration: 3000,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+      background: "#ef4444",
+      },
+    }).showToast()
+    
+    return;
+  }
+
   if(cart.length === 0)return;
   if(addressInput.value === ""){
     addressWarn.classList.remove("hidden")
     addressInput.classList.add("border-red-500")
     return;
   }
+
+  cartItems = cart.map((item) => {
+    return(
+          `$(item.name)Quantidade:  (${item.quantity}) Preço: R$ ${item.price} |`
+  )
+  }).join("")
+  const message = encodeURIComponent(cartItems);
+  const phone = "15997992178"
+
+  window.open('https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}',"blank")
+
+  cart =[];
+  updateCartModal();
 })
+
+
+function checkRestaurantOpen(){
+  const data = new Date();
+  const hora = data.getHours();
+  return hora >= 18 && hora < 22;
+
+}
+
+
+const spanItem = document.getElementById("data-span")
+const isOpen = checkRestaurantOpen();
+
+if(isOpen){
+  spanItem.classList.remove("bg-red-500")
+  spanItem.classList.add("bg-green-600")
+}else{
+  spanItem.classList.remove("bg-green-600")
+  spanItem.classList.add("bg-red-500")
+}
+
 
